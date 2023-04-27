0005a4d1e73f3fdf3b6b9c5e05c612fabf6a71a0
SiapaLupa <muhammadilyasafadhlih@gmail.com>
2023-04-28 06:58:40 +0700

    fix result

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const MAIN = document.querySelector("main");
 const INPUTS = document.querySelectorAll("main > input");
 const CONVERTER = ["decimal", "octal", "hexadecimal", "binary"];
 const CONVERTER_BASE = [10, 8, 16, 2];
+const PREFIX = ["", "0o", "0x", "0b"];
 const COLOR = ["#DDFFBB", "#C7E9B0", "#B3C99C", "#A4BC92"];
 const HEX_ADDITIONAL = ["a", "b", "c", "d", "e", "f"];
 
@@ -22,24 +23,9 @@ CONVERTER.forEach(function (converter, index) {
 });
 
 function convert({ value, id: converter }, to) {
-    let convertedResult;
-    switch (converter) {
-        case "decimal":
-            convertedResult = window.decimal(value, to);
-            break;
-        case "octal":
-            convertedResult = window.octal(value, to);
-            break;
-        case "hexadecimal":
-            convertedResult = window.hexadecimal(value, to);
-            break;
-        case "binary":
-            convertedResult = window.binary(value, to);
-            break;
-        default:
-            convertedResult = null;
-    }
-    return convertedResult;
+    let converted = PREFIX[CONVERTER.indexOf(converter)].concat(value);
+    let base = CONVERTER_BASE[CONVERTER.indexOf(to)];
+    return Number(converted).toString(base);
 }
 
 function keyup(event) {
@@ -58,62 +44,30 @@ function keydown(event, converter) {
             validInput = /[\da-f]/gi;
             validInput = new RegExp(
                 validInput.source + "|" + validInputPattern.source,
-                "gi",
+                "gi"
             );
             break;
         case "binary":
             validInput = /[0-1]/gi;
             validInput = new RegExp(
                 validInput.source + "|" + validInputPattern.source,
-                "gi",
+                "gi"
             );
             break;
         case "octal":
             validInput = /[0-7]/gi;
             validInput = new RegExp(
                 validInput.source + "|" + validInputPattern.source,
-                "gi",
+                "gi"
             );
             break;
         default:
             validInput = /\d/gi;
             validInput = new RegExp(
                 validInput.source + "|" + validInputPattern.source,
-                "gi",
+                "gi"
             );
     }
     var isValid = validInput.test(event.key);
     if (!isValid) event.preventDefault();
-}
-
-function decimal(value, to) {
-    let base = CONVERTER_BASE[CONVERTER.indexOf(to)];
-    return Number(value).toString(base);
-}
-function hexadecimal(value, to) {
-    let base = CONVERTER_BASE[CONVERTER.indexOf(to)];
-    let split = value.split('').map(value => {
-        find = String(HEX_ADDITIONAL.find(_ => value === _))
-        if (find === "undefined") return value
-        return find.charCodeAt() - 87
-    });
-    let result = [];
-    switch (to) {
-        case "decimal":
-            result = split.map((value, index) => {
-                return Number(value) * Math.pow(16, split.length - 1 - index)
-            }).reduce((prev, curr) => prev + curr);
-            break;
-        case "octal":
-        default:
-            result = ["1", "99"];
-            break;
-    }
-    return result
-}
-function octal(params) {
-    return 199;
-}
-function binary(params) {
-    return 199;
 }
